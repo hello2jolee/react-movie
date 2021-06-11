03d718f9d0c6395a5cd657d03eec93c3aedea547
jioun.lee.dev <jioun.lee@nts-corp.com>
2021-06-11 17:55:56 +0900

    #1 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
 import React from "react";
-
-function Food({ fav, picture }) {
-  return (
-    <div>
-      <h2>I like {fav}</h2>
-      <img src={picture} alt={fav} />
-    </div>
-  );
-}
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
@@ -21,29 +13,56 @@ const foodILike = [
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 4.9,
   },
   {
     id: 3,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 4.8,
   },
   {
     id: 4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 5.5,
   },
   {
     id: 5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 4.7,
   },
 ];
 
+function Food({ fav, picture, rating }) {
+  return (
+    <div>
+      <h2>I like {fav}</h2>
+      <h4>{rating} / 5</h4>
+      <img src={picture} alt={fav} />
+    </div>
+  );
+}
+
+Food.propTypes = {
+  fav: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
+
 function renderFood(dish) {
-  return <Food key={dish.id} fav={dish.name} picture={dish.image} />;
+  return (
+    <Food
+      key={dish.id}
+      fav={dish.name}
+      picture={dish.image}
+      rating={dish.rating}
+    />
+  );
 }
 
 function App() {
