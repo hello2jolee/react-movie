adc585d9a8e7a39671fe68b489fa451587845de3
jioun.lee.dev <jioun.lee@nts-corp.com>
2021-06-13 23:08:10 +0900

    #3 all you need to know about state

--- src/App.js
+++ src/App.js
@@ -1,77 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.8,
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 5.5,
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.7,
-  },
-];
-
-function Food({ fav, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {fav}</h2>
-      <h4>{rating} / 5</h4>
-      <img src={picture} alt={fav} />
-    </div>
-  );
-}
-
-Food.propTypes = {
-  fav: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
-
-function renderFood(dish) {
-  return (
-    <Food
-      key={dish.id}
-      fav={dish.name}
-      picture={dish.image}
-      rating={dish.rating}
-    />
-  );
-}
-
-function App() {
-  return (
-    <div>
-      <h1>HELLO</h1>
-      {foodILike.map(renderFood)}
-    </div>
-  );
+class App extends React.Component {
+  // put data that will change
+  state = {
+    count: 0,
+  };
+  add = () => {
+    // every time you call "setState" react will render with the new state
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  // don't use "this.state" => 성능상 이슈가 발생할 수 있음
+  minus = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
